58afd8fe5e667a90ce33c0ffd49ecb9c6f9c51e5
Choi Jiwon <winning1993@naver.com>
2020-05-02 23:12:35 +0900

    가격 표시, 선택된 자리 표시, localStraged 완료

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const seats = document.querySelectorAll(".seat");
-const chosenSeats = document.getElementById("chosenSeats");
+const selectedNum = document.getElementById("selectedNum");
+const price = document.getElementById("price");
+const movieSelect = document.getElementById("movieSelect");
 
 let i = 0;
 let selectedSeats = [];
@@ -11,9 +13,11 @@ seats.forEach( seat => {
     seat.index = i;
     console.log(seat, i);
     JSON.parse(localStorage.getItem("selectedSeat")).map(saved => {
-        if(seat.index === saved)
-    return seat.style.backgroundColor = "#6FEAF6";
+        if(seat.index === saved){
+    seat.style.backgroundColor = "#6FEAF6";
     seat.addEventListener("click", handleCancel);
+    
+        }
     })
     i+=1;
 });
@@ -26,7 +30,7 @@ function handleClick(e){
     localStorage.setItem("selectedSeat", JSON.stringify(selectedSeats));
     e.target.removeEventListener("click", handleClick);
     e.target.addEventListener("click", handleCancel);
-    chosenSeats.innerHTML = `You've chose ${e.currentTarget.index}`
+    calculate();
 }
 
 function handleCancel(e){
@@ -36,11 +40,47 @@ function handleCancel(e){
     localStorage.setItem("selectedSeat", JSON.stringify(selectedSeats));
     e.target.removeEventListener("click", handleCancel);
     e.target.addEventListener("click", handleClick);
+    calculate();
+}
+
+function handleSelect(e){
+    console.log(`selected something:${e.target.value}`);
+    console.log(e.target.value);
+    localStorage.setItem("selectedMovie", e.target.value);
+
+    const length = JSON.parse(localStorage.getItem("selectedSeat")).length;
+    if(e.target.value === "1"){
+        price.innerHTML = length * 10;
+        localStorage.setItem("moviePrice", 10);
+    }
+    else if(e.target.value === "2"){
+        price.innerHTML = length * 12;
+        localStorage.setItem("moviePrice", 12);
+    }
+    else if(e.target.value === "3"){
+        price.innerHTML = length * 8;
+        localStorage.setItem("moviePrice", 8);
+    }
+    else if(e.target.value === "4"){
+        price.innerHTML = length * 9;        
+        localStorage.setItem("moviePrice", 9);
+
+    }
+    
+}
+
+function calculate(){
+   const length = JSON.parse(localStorage.getItem("selectedSeat")).length;
+   selectedNum.innerHTML = length;
+   price.innerHTML = length * localStorage.getItem("moviePrice");
+   movieSelect.value = localStorage.getItem("selectedMovie");
 }
 
 
 
 function init(){
+    calculate();
+    movieSelect.addEventListener("change", handleSelect);
     
 }
 
